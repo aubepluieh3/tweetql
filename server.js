feeb8bd68946758f0a84541ac1800a6994d06cc9
aubepluieh3 <aubepluieh3@naver.com>
2022-10-05 15:33:25 +0900

    Non Nullable Fields

--- server.js
+++ server.js
@@ -2,24 +2,28 @@ import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
   type User {
-    id: ID
-    username: String
+    id: ID!
+    username: String!
+    firstName: String!
+    lastName: String
   }
   type Tweet {
-    id: ID
-    text: String
-    author: User
+    id: ID!
+    text: String!
+    author: User!
   }
   type Query {
-    allTweets: [Tweet]
-    tweet(id: ID): Tweet
+    allTweets: [Tweet!]!
+    tweet(id: ID!): Tweet
   }
-
   type Mutation {
-    postTweet(text: String, userId: ID): Tweet
-    deleteTweet(id:ID): Boolean
+    postTweet(text: String!, userId: ID!): Tweet!
+    deleteTweet(id: ID!): Boolean!
   }
 `;
+// GET /api/v1/tweets
+// POST DELETE PUT /api/v1/tweets
+// GET /api/v1/tweet/:id
 
 const server = new ApolloServer({ typeDefs });
 
